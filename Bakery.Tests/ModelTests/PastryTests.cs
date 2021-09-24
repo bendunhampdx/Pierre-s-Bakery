d15cf520e217b5d39571ba5f9be0476c910ecd70
bendunhampdx <bendunhampdx@gmail.com>
2021-09-24 13:46:36 -0700

    pass test for pastry determining cost of multipple  pastries with discount

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -24,8 +24,8 @@ namespace Bakery.Tests
       Assert.AreEqual(resultCost, newPastry.Cost);
     }
 
-        [TestMethod]
-    public void SetPastryDiscount_PastryDiscountAccountedFor_Double()
+    [TestMethod]
+    public void SetPastryDiscount_PastryDiscountAccountedForAndQuantityIsLessThanThree_Double()
     {
     //Arrange
     int quantityOrdered = 1;
@@ -39,6 +39,23 @@ namespace Bakery.Tests
     //Assert
       Assert.AreEqual(resultQuantityOrdered, newPastry.QuantityOrdered);
       Assert.AreEqual(resultCost, 2);
+    }
+
+    [TestMethod]
+    public void SetPastryDiscount_PastryDiscountAccountedFor_Double()
+    {
+    //Arrange
+    int quantityOrdered = 9;
+    double cost = 15;
+
+    //Act
+      Pastry newPastry = new Pastry(quantityOrdered, cost);
+      int resultQuantityOrdered = newPastry.QuantityOrdered;
+      double resultCost = newPastry.Cost;
+
+    //Assert
+      Assert.AreEqual(resultQuantityOrdered, newPastry.QuantityOrdered);
+      Assert.AreEqual(resultCost, 15);
     }
   }
 }
